a89fe6f20648211e8479e27bac6106812b232900
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-04-25 10:28:21 +0100

    Update SecurityConfig.java

--- src/main/java/com/sales/security/SecurityConfig.java
+++ src/main/java/com/sales/security/SecurityConfig.java
@@ -13,11 +13,17 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers("/**")
-			.authenticated()
-			.and()
-			.formLogin();
+		http
+	        .authorizeRequests()
+	            .antMatchers("/")
+	            .permitAll()
+	            .anyRequest()
+	            .authenticated()
+	            .and()
+	        .formLogin()
+	        	.and()
+	        .logout()
+	        	.logoutSuccessUrl("/");
 	}
 
 	@Bean
